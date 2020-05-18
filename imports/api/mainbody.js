7e184f004e6a8ab685082d44576f84400a64a05a
RavenXavier <k.munro1998@gmail.com>
2020-05-18 18:49:55 -0400

    Checkbox features added

--- imports/api/mainbody.js
+++ imports/api/mainbody.js
@@ -1,7 +1,24 @@
 Template.mainBody.helpers({
 	publicTask(){
 		return publicTasksdb.find();
+	},
+	checked: function(){
+		var myCompleted = this.completed;
+		if(myCompleted){
+			return "checked";
+		} else {
+			return "";
+		}
+	},
+	userField(){//ceck to see if image has a saved user
+		if (!(this.createdBy == undefined)){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
+
 });
 
 Template.mainBody.events({
@@ -14,5 +31,24 @@ Template.mainBody.events({
 	},
 	'click .js-editTask'(event, instance){
 		console.log(this._id);
+	},
+	'change [type=checkbox]': function(){
+		var myId = this._id;
+		var myCompleted = this.completed;
+		if(myCompleted){
+			publicTasksdb.update({_id:myId}, 
+			{$set:{
+				"completed": false
+				// "completed": completed
+			}});
+			console.log("Task marked as incomplete: "+ myId);
+		} else{
+			publicTasksdb.update({_id:myId}, 
+			{$set:{
+				"completed": true
+				// "completed": myCompleted
+			}});
+			console.log("Task marked as complete: "+ myId);
+		}
 	}
 });
