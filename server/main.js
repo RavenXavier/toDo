3fb2f024cef26af7e1bf6cb42954649ef0c1e0f9
RavenXavier <k.munro1998@gmail.com>
2020-05-20 12:48:25 -0400

    Publish/Subscribe/Allow

--- server/main.js
+++ server/main.js
@@ -4,3 +4,11 @@ import '../lib/collection.js';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+Meteor.publish('taskLists', function () {
+  return publicTasksdb.find({}, {
+    // fields: { createdById: 0 } //hide this feild
+  });
+  return privateTasksdb.find({}, {
+    // fields: { createdById: 0 } //hide this feild
+  });
+});
