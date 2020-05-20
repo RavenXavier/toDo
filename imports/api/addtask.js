9f85bf3bb35717bfeb309c61d30ae8d012461bc3
RavenXavier <k.munro1998@gmail.com>
2020-05-20 12:41:18 -0400

    Private/Public option (only pub saves&shows info)

--- imports/api/addtask.js
+++ imports/api/addtask.js
@@ -1,14 +1,59 @@
+// Template.addTask.helpers({
+// 	userField(){//ceck to see if image has a saved user
+// 		if (!(this.createdBy == undefined)){
+// 			return true;
+// 		}
+// 		else{
+// 			return false;
+// 		}
+// 	}
+// });
+
 Template.addTask.events({
+// 'click #p1': function(event){
+//   	var taskLoc = $("#p1:checked").val();
+//   	console.log(taskLoc);
+//   	// publicTasksdb.insert({
+//    //  	"status": taskLoc
+//   	// });
+// 	},
+// 	'click #p2': function(event){
+//     var taskLoc = $("#p2:checked").val();
+//     console.log(taskLoc);
+//     // privateTasksdb.insert({
+//   		// "status": taskLoc
+//     // });
+// 	},
+
 	'click .js-saveMe'(event, instance){
-		console.log("add a task");
 		var myList = $("#listName").val();
 		var myTask = $("#taskName").val();
-		publicTasksdb.insert({
-			"list": myList,
-			"task": myTask,
-			"createdOn": new Date().getTime()
-		});
-		console.log("saving");
+		var myStat1 = $("#p1:checked").val();	
+		var myStat2 = $("#p2:checked").val();			
+		// var myStat2 = $("#p2").val('status');
+		if (myStat1){
+			console.log("saved to: " + myStat1);
+			publicTasksdb.insert({
+				"list": myList,
+				"task": myTask,
+				"completed": false,
+				"status": myStat1,
+				"createdOn": new Date().getTime(),
+				"createdBy": Meteor.users.findOne({_id:Meteor.userId()}).username,
+	 			"createdById": Meteor.userId()
+			});
+		}else if(myStat2) {
+			console.log("saved to: " + myStat2);
+			privateTasksdb.insert({
+				"list": myList,
+				"task": myTask,
+				"completed": false,
+				"status": myStat2,
+				"createdOn": new Date().getTime(),
+				"createdBy": Meteor.users.findOne({_id:Meteor.userId()}).username,
+	 			"createdById": Meteor.userId()
+			});
+		}
 		$("#AddTaskModal").modal("hide");
 		var myList = $("#listName").val("");
 		var myTask = $("#taskName").val("");
@@ -18,22 +63,9 @@ Template.addTask.events({
 		var myList = $("#listName").val("");
 		var myTask = $("#taskName").val("");
 	},
-	'click .js-newTask'(event, instance){
-		console.log("new");
-		var myTask = $("#taskName").val();
-		testCodedb.insert({
-			"task": myTask
-		
-		});
-	},
 	'click .js-delTask'(event, instance){
 		var myId = this._id;
 		testCodedb.remove({_id:myId});
 	}
-});
 
-Template.addTask.helpers({
-	taskNew(){
-		return testCodedb.find();
-	}
 });
